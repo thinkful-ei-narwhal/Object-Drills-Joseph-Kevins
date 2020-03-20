061f9c899745625bfb50657ac1392147258af66f
Joseph Rivera <josephr98@gmail.com>
2020-03-20 00:07:48 -0400

    finished drill 7

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-function createCharacter(name, nickname, race, origin, attack, defense) {
+function createCharacter(name, nickname, race, origin, attack, defense, weapon) {
   return {
     name,
     nickname,
@@ -7,19 +7,34 @@ function createCharacter(name, nickname, race, origin, attack, defense) {
     attack,
     defense,
     describe() {
-      return `${this.name} is a ${this.race} from ${this.origin}.`
+      return `${this.name} is a ${this.race} from ${this.origin} who uses ${this.weapon}.`
     },
     evaluateFight(char) {
       return `Your opponent takes ${this.attack > char.defense ? this.attack - char.defense : 0} damage and you receive ${char.attack > this.defense ? char.attack - this.defense : 0} damage`;
-    }
+    },
+    weapon,
   }
 }
 
-const gandalf = createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6);
-const bilbo = createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1);
-const frodo = createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2);
-const aragorn = createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8);
-const legolas = createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5);
+const characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6, 'wizard staff'),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1, 'the Ring'),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2, 'a String and Barrow Blade'),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8, 'an Anduril'),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5, 'a Bow and Arrow'),
+]
 
-console.log(frodo.describe());
-console.log(gandalf.evaluateFight(legolas));
+characters.push(createCharacter('Arwen Undomiel', 'arwen', 'Half-Elf', 'Rivendell', 9, 4, 'a Hadhafang'));
+
+const aragorn = characters.find(obj => obj.nickname == 'aragorn');
+let describeAragorn = aragorn.describe();
+
+// console.log(describeAragorn);
+
+const hobbits = characters.filter(obj => obj.race == 'Hobbit');
+// hobbits.forEach(obj => console.log(obj));
+
+const strongChars = characters.filter(obj => (obj.attack > 5) ? true : false);
+// console.log(strongChars);
+
+console.log(characters[3].describe());
